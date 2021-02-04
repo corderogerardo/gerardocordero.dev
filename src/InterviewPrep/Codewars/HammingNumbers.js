3431e97947f58a9cf7f2a87a8ef67fdd7090727d
Gerardo Cordero <cordero.gerard@gmail.com>
2021-02-04 11:05:59 -0500

    CodeWars: Hamming Numbers WIP

--- src/InterviewPrep/Codewars/HammingNumbers.js
+++ src/InterviewPrep/Codewars/HammingNumbers.js
@@ -1,44 +1,45 @@
-function isHammingNumber(num) {
-  while (num % 5 === 0) num /= 5;
-  while (num % 3 === 0) num /= 3;
-  while (num % 2 === 0) num /= 2;
-  return num == 1;
-}
+var _primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
 
-function hamming(n) {
-  // TODO: Program
-  let succession = [1];
-  let length = succession.length;
-  let candidate = n;
-  while (length < n) {
-    if (isHammingNumber(candidate)) {
-      succession[length] = candidate;
-      length++;
+// ToDo: https://www.codewars.com/kata/526d84b98f428f14a60008da/train/javascript
+
+function* build_hamming() {
+  let queues = { 2: [], 3: [], 5: [] };
+  let next_ham = 1;
+  while (true) {
+    yield next_ham;
+
+    for (let base in queues) {
+      console.log("base ", base);
+      console.log("next_ham ", next_ham);
+      if (!!queues[base]) {
+        queues[base].push(next_ham * base);
+      } else {
+        queues[base] = [];
+        queues[base].push(next_ham * base);
+      }
     }
-    candidate++;
-  }
-  return length;
-}
 
-function isHammingNumber(num) {
-  if (num == 1) return num == 1;
-  while (num % num === 0) num /= num;
-  while (num % 3 === 0) num /= 3;
-  while (num % 2 === 0) num /= 2;
-}
+    console.log("queues ", queues);
 
-function hamming(n) {
-  // TODO: Program
-  let succession = [1];
-  let length = succession.length;
-  let candidate = n;
-  if (!!succession[candidate - 1]) return succession[candidate - 1];
-  while (length < n) {
-    if (isHammingNumber(candidate)) {
-      succession[length] = candidate;
-      length++;
+    next_ham = Object.values(queues).reduce(function (min, value) {
+      return Math.min(min, value);
+    });
+
+    for (let base in queues) {
+      if (queues[base][0] == next_ham) queues[base].shift();
     }
-    candidate++;
   }
-  return length;
 }
+function hamming(n) {
+  if (n > 0 && n <= 6) return n;
+  let ham = build_hamming();
+  let first20 = [];
+  for (let i = 1; i <= n; i++) {
+    first20.push(ham.next());
+  }
+  console.log("first20 ", first20);
+  return first20[first20.length - 1];
+}
+
+const result = hamming(8);
+console.log("Result ", result);
