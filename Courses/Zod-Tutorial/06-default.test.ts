2c55fcec56fd2beec1c7daf71876a614073b68f5
Gerardo Cordero <cordero.gerard@gmail.com>
2023-03-07 00:03:45 -0400

    Default vs Opcional, input vs infer, Input Output

---
+++ Courses/Zod-Tutorial/06-default.test.ts
@@ -0,0 +1,21 @@
+// CODE
+
+import { expect, it } from "vitest";
+import { validateFormInput } from "./06-default.problem";
+
+it("Should include keywords if passed", async () => {
+  const result = validateFormInput({
+    repoName: "mattpocock",
+    keywords: ["123"],
+  });
+
+  expect(result.keywords).toEqual(["123"]);
+});
+
+it("Should automatically add keywords if none are passed", async () => {
+  const result = validateFormInput({
+    repoName: "mattpocock",
+  });
+
+  expect(result.keywords).toEqual([]);
+});
